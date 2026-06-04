8e933012420176f0e964905def06b8e312d4ea3b
Bernardo Vieira <bernardosvieira25@gmail.com>
2026-06-04 10:48:24 -0300

    refactor: move training dataset to CSV file

--- src/main.py
+++ src/main.py
@@ -3,48 +3,17 @@ from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
 
-# 1. Dados de Treinamento
-data = {
-    "Febre": [
-        "Sim", "Sim", "Nao", "Nao",
-        "Sim", "Nao", "Sim", "Nao"
-    ],
+# 1. Dados de Treinamento no arquivo symptoms_dataset.csv
 
-    "Tosse": [
-        "Sim", "Nao", "Sim", "Nao",
-        "Sim", "Nao", "Nao", "Sim"
-    ],
-
-    "DorPeito": [
-        "Nao", "Nao", "Nao", "Nao",
-        "Sim", "Nao", "Sim", "Sim"
-    ],
-
-    "Urgencia": [
-        "Media",
-        "Baixa",
-        "Baixa",
-        "Baixa",
-        "Alta",
-        "Baixa",
-        "Alta",
-        "Media"
-    ]
-}
-df = pd.DataFrame(data)
+df = pd.read_csv("data/symptoms_dataset.csv")
+print(df.head())
 
 # 2. Pré-processamento (Codificação Categórica)
-le_febre = LabelEncoder()
-le_tosse = LabelEncoder()
-le_dor_peito = LabelEncoder()
-le_target = LabelEncoder()
 
-df["febre"] = le_febre.fit_transform(df["Febre"])
-df["tosse"] = le_tosse.fit_transform(df["Tosse"])
-df["dor_peito"] = le_dor_peito.fit_transform(df["DorPeito"])
+le_target = LabelEncoder()
 df["Urgencia"] = le_target.fit_transform(df["Urgencia"])
 
-X = df[["febre", "tosse", "dor_peito"]]
+X = df[["Febre", "Tosse", "DorPeito"]]
 y = df["Urgencia"]
 
 # 3. Treinamento do Modelo (Critério ID3: Entropia)
@@ -54,7 +23,10 @@ clf.fit(X, y)
 # --- SEÇÃO DE TESTE (SAÍDAS) ---
 print("--- Simulador de Decisão ---")
 # Exemplo: Paciente com Febre=Sim, Tosse=Não e DorPeito=Não
-nova_entrada = [[1, 0, 0]]
+nova_entrada = pd.DataFrame(
+    [[1, 0, 0]],
+    columns=["Febre", "Tosse", "DorPeito"]
+)
 predicao = clf.predict(nova_entrada)
 resultado = le_target.inverse_transform(predicao)
 
@@ -64,7 +36,9 @@ print(f"Resultado para Febre: Sim, Tosse: Nao, Dor no Peito: Nao: {resultado[0]}
 plot_tree(
     clf,
     feature_names=X.columns,
-    class_names=["Baixa", "Media", "Alta"],
+    class_names=le_target.classes_,
     filled=True,
 )
+print(le_target.classes_)
+
 plt.show()
